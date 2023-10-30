eb93441ccb13af204d9927429ac48994d1bcde29
Roland Björndal <bjorndahl95@hotmail.com>
2023-10-30 14:27:01 +0100

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express(),
   port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/', (_request, response) => {
   response.send({ hello: 'World' })
